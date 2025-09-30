c71e4e0b38fdca1dee114bd47b67c0f646e3004f
splashdexstudios <splashdexstudios@gmail.com>
2025-09-30 03:31:13 +0000

    nice

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,9 +1,12 @@
-{pkgs}: {
+{ pkgs, ... }: {
   channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
     pkgs.haskellPackages.snap-templates
-    pkgs.openssh 
+    pkgs.openssh
+    (pkgs.google-cloud-sdk.withExtraComponents [
+      pkgs.google-cloud-sdk.components.cloud-datastore-emulator
+    ])
   ];
   idx.extensions = [
     "angular.ng-template"
@@ -25,6 +28,14 @@
           "--disable-host-check"
         ];
         manager = "web";
+      };
+      android = {
+        command = [
+          "sh"
+          "-c"
+          "npm i && npx @capacitor/android@5.7.4 && npm i -D @capacitor/cli@5.7.4 && npx cap add android && npx cap sync android && npx cap open android"
+        ];
+        manager = "process";
       };
     };
   };
