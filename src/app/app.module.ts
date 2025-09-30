4a35b10fa66fd4e63972fb4c8fb51ce9c06574d6
splashdexstudios <splashdexstudios@gmail.com>
2025-09-30 16:39:52 +0000

    Changes before Firebase Studio auto-run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from "@environments/environment";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent, UpdateProfilComponent],
@@ -25,7 +26,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
           scrollAssist: false
       }),
       AppRoutingModule,
-      ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+      ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+      AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [
      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
